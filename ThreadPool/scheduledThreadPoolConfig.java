dc2d2150fc4872340376f3065a5686c56a3c5ebd
727072938@qq.com <727072938@qq.com>
2024-09-19 16:34:19 +0800

    定时线程池配置

--- ThreadPool/scheduledThreadPoolConfig.java
+++ ThreadPool/scheduledThreadPoolConfig.java
@@ -1,10 +1,20 @@
-package PACKAGE_NAME;/**
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+/**
  * @Author Colin
  * @Date 2024/9/19 14:28
  * @PackageName:PACKAGE_NAME
  * @ClassName: scheduledThreadPoolConfig
- * @Description: TODO
+ * @Description: 定时、延迟线程池配置
  * @Version 1.0
  */
 public class scheduledThreadPoolConfig {
+    private static ScheduledThreadPoolExecutor sceduledThreadPool;
+    static {
+        int corePoolSize = 10;
+        sceduledThreadPool = new ScheduledThreadPoolExecutor(corePoolSize);
+    }
+    public static ScheduledThreadPoolExecutor getExecutor(){
+        return sceduledThreadPool;
+    }
 }
