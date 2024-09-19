8fd618c70045a7fa117262e4091b6ad2fb89d833
727072938@qq.com <727072938@qq.com>
2024-09-19 16:28:49 +0800

    只有入参任务样例代码

--- ThreadPool/inputParametersTask.java
+++ ThreadPool/inputParametersTask.java
@@ -1,10 +1,36 @@
-package PACKAGE_NAME;/**
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
  * @Author Colin
  * @Date 2024/9/19 10:34
  * @PackageName:PACKAGE_NAME
  * @ClassName: inputParametersTask
- * @Description: TODO
+ * @Description: 只有传入参数的任务
  * @Version 1.0
  */
 public class inputParametersTask {
+
+    static class ParameterizedTask implements Runnable {
+        private int taskId;
+        private String taskName;
+        public ParameterizedTask(int taskId, String taskName) {
+            this.taskId = taskId;
+            this.taskName = taskName;
+        }
+        @Override
+        public void run() {
+            //Task code
+            System.out.println("Task " + taskId + " " + taskName + " is running");
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor job = ThreadPoolConfig.getExecutor();
+        for (int i = 0; i < 100; i++) {
+            job.execute(new ParameterizedTask(i, "task"));
+            System.out.println("执行异步任务 - " + Thread.currentThread().getName());
+        }
+        job.shutdown();
+    }
+
 }
