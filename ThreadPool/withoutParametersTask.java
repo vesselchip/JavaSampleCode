cfa8d883c17446e5f096eb4188e7c8786617812b
727072938@qq.com <727072938@qq.com>
2024-09-19 16:27:53 +0800

    无参数任务样例

--- ThreadPool/withoutParametersTask.java
+++ ThreadPool/withoutParametersTask.java
@@ -1,10 +1,21 @@
-package PACKAGE_NAME;/**
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
  * @Author Colin
  * @Date 2024/9/19 10:26
  * @PackageName:PACKAGE_NAME
  * @ClassName: withoutParametersTask
- * @Description: TODO
+ * @Description: 没有参数的任务
  * @Version 1.0
  */
 public class withoutParametersTask {
+    public static void main(String[] args) {
+        ThreadPoolExecutor job = ThreadPoolConfig.getExecutor();
+        for (int i = 0; i < 100; i++) {
+            job.execute(()-> {
+                System.out.println("执行异步任务 - " + Thread.currentThread().getName());
+            });
+        }
+        job.shutdown();
+    }
 }
